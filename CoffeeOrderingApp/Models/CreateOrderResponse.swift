6e0746e4eb58c62a50f63c69c09bc10d0535ef26
fatehkhann <fatehkhann@gmail.com>
2020-05-09 17:12:50 +0500

    Major Changes

---
+++ CoffeeOrderingApp/Models/CreateOrderResponse.swift
@@ -0,0 +1,13 @@
+//
+//  CreateOrderResponse.swift
+//  CoffeeOrderingApp
+//
+//  Created by Fateh Khan on 08/05/2020.
+//  Copyright © 2020 Zarrar Company. All rights reserved.
+//
+
+import Foundation
+
+struct CreateOrderReponse: Decodable {
+    let success: Bool
+}
